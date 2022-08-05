26e026085b052236c702f58cd27b12e16c76ed16
Gitarplayer <christian_bucheli@hotmail.com>
2022-08-05 06:53:59 +0200

    fix folder structure

--- GhostDeleteAllPosts/index.js
+++ GhostDeleteAllPosts/index.js
@@ -1,9 +1,12 @@
 module.exports = async function (context, req) {
+    console.log(process.env.GHOST_URL);
+    console.log(process.env.GHOST_ADMIN_KEY);
+    console.log(process.env.GHOST_API_VERSION);
     const GhostAdminAPI = require('@tryghost/admin-api');
     const api = new GhostAdminAPI({
-        url: process.env["GHOST_URL"],
-        key: process.env["GHOST_ADMIN_KEY"],
-        version: process.env["GHOST_API_VERSION"]
+        url: process.env.GHOST_URL,
+        key: process.env.GHOST_ADMIN_KEY,
+        version: process.env.GHOST_API_VERSION
     });
     api.posts
         .browse({limit: "all"})
@@ -14,7 +17,7 @@ module.exports = async function (context, req) {
             });
         })
         .catch((err) => {
-            console.error(err);
+            console.error(err);debug 
         });    
     context.res = {
         // status: 200, /* Defaults to 200 */
